dd722b7dc633e480b2d82037e341a127ec4d9809
Jake977 <54539983+Jake977@users.noreply.github.com>
2019-11-23 06:50:04 +0300

    brands block

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 let toggleButton = document.querySelector('.btn_show_hide');
-//let elementsContainer = document.querySelector('.all_brands_logos');
-let allblocks = document.querySelectorAll('.brand_item');
+let brandsCollection = document.querySelector('.all_brands_logos');
+let allblocks = brandsCollection.querySelectorAll('.brand_item');
 let toggle = "hide";
 
 toggleButton.addEventListener('click', function(){
 
     if (toggle === 'hide') {
+
         for (let i = 0; i < allblocks.length; i++) {
-            allblocks[i].classList.add('show2');
+            allblocks[i].classList.add('show');
         }
+
         toggle = 'show';
         toggleButton.textContent = "Cкрыть";
         toggleButton.classList.remove('icon_arrows_down');
         toggleButton.classList.add('icon_arrows_up');
     } else {
+
         for (let i = 0; i < allblocks.length; i++) {
-            allblocks[i].classList.remove('show2');
+            allblocks[i].classList.remove('show');
         }
+
         toggle = 'hide';
         toggleButton.textContent = "Показать все";
         toggleButton.classList.remove('icon_arrows_up');
