712dc4d8ebe245a764a466afcf1bdeeb80a45329
Jake977 <54539983+Jake977@users.noreply.github.com>
2019-11-25 21:31:54 +0300

    links, icons, overflow fix

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
-let toggleButton = document.querySelector('.btn_show_hide');
 let brandsCollection = document.querySelector('.all_brands_logos');
-let allblocks = brandsCollection.querySelectorAll('.brand_item');
+let toggleButton = document.querySelector('.btn_show_hide');
+let buttonTextShowall = toggleButton.querySelector('.textShowall');
+let buttonTextHide = toggleButton.querySelector('.textHide');
 
 toggleButton.addEventListener('click', function(){
 
-    for (let i = 0; i < allblocks.length; i++) {
-        allblocks[i].classList.toggle('show');
-    }
-
+    brandsCollection.classList.toggle('overflow_hide');
     toggleButton.classList.toggle('icon_arrows_up');
     toggleButton.classList.toggle('icon_arrows_down');
-
-    if (toggleButton.textContent === 'Cкрыть') {
-        toggleButton.textContent = "Показать все";
-    } else {
-        toggleButton.textContent = "Cкрыть";
-    }
+    buttonTextShowall.classList.toggle('show');
+    buttonTextHide.classList.toggle('show');
 })
